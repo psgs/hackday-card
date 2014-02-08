64253ab96252bdbfc421faa8d652f1f84c600a90
Alyson La <alyson@github.com>
2014-02-08 14:43:54 -0800

    inital commit

---
+++ javascripts/card-flip.js
@@ -0,0 +1,24 @@
+// /*
+//  * $(document).ready() is a helper function that waits until the jQuery library is fully loaded in your Browser before it tries to use it.
+// */
+// $(document).ready(function() {
+  
+//    * This function waits and listens for a "click" to happen on the div with the class name "hackentines-day-card-container"
+  
+//   // $(".hackentines-day-card-container").on("click", function() {
+//   //   $(".card").fadeToggle();
+//     /*
+//       If you like, Try this instead!
+//       $(".card").slideToggle();
+//       or, try this!
+//       $(".card").fadeToggle();
+//     */
+//   });
+// });
+
+$('.flip').click(function(){
+  $(this).find('.card').addClass('flipped').mouseleave(function(){
+      $(this).removeClass('flipped');
+        });
+  return false;
+});
